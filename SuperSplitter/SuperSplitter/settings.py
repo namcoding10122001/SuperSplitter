6c08706149a3c012078a3f959d37af3f948de8f4
Nam <namcoding10122001@gmail.com>
2022-02-01 19:37:53 +0100

    added login

--- SuperSplitter/SuperSplitter/settings.py
+++ SuperSplitter/SuperSplitter/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'SuperSplitter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# config/settings.py
+LOGIN_REDIRECT_URL = '/'
