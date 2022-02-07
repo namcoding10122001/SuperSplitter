0443efe568b5e0e93075c92c09124c7beef70666
Nam <namcoding10122001@gmail.com>
2022-02-07 10:44:23 +0100

    Cleared cache and added css to login

--- SuperSplitter/SuperSplitter/settings.py
+++ SuperSplitter/SuperSplitter/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),) # new
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles')) # new
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
