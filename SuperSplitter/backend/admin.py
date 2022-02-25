7c2654816c689b647ac11cc20fad6e508713dc89
Apple0410 <ngominh041097@gmail.com>
2022-02-26 01:27:27 +0700

    Add models to admin.py

--- SuperSplitter/backend/admin.py
+++ SuperSplitter/backend/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import User, Session, Tag
+from .models import User, Session, Tag, Currency, Payment
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Session)
 admin.site.register(Tag)
+admin.site.register(Currency)
+admin.site.register(Payment)
+
