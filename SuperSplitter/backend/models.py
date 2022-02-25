a15d5d201557d57837cd1f84dd7e91e00d63da0a
Apple0410 <ngominh041097@gmail.com>
2022-02-26 01:25:29 +0700

    Create Django Model

--- SuperSplitter/backend/models.py
+++ SuperSplitter/backend/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 
 class User(models.Model):
     name = models.CharField(max_length=200)
     dob = models.DateField()
-    email = models.TextField(max_length=1000)
+    email = models.EmailField()
+    avatar = models.ImageField(null=True, blank=True, upload_to='storage')
     created = models.DateTimeField(auto_now_add=True)
     id = models.AutoField(primary_key=True, unique=True, editable=False)
 
@@ -13,23 +15,58 @@ class User(models.Model):
 
 
 class Session(models.Model):
-    spender = models.ForeignKey(User, related_name='%(class)s_spender', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, default="Session")
+    creator = models.ForeignKey(User, related_name='%(class)s_creator', on_delete=models.SET_NULL, null=True)
     participants = models.ManyToManyField(User, blank=True)
     date_time = models.DateTimeField()
     place_name = models.TextField(max_length=1000, null=True, blank=True)
     total_spent = models.FloatField()
+    currency = models.ForeignKey('Currency', editable=True, on_delete=models.SET_NULL, null=True)
     tags = models.ManyToManyField('Tag', blank=True)
+    last_updated = models.DateTimeField(editable=True, default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     id = models.AutoField(primary_key=True, unique=True, editable=False)
+    finish = models.BooleanField(default=False, editable=True, null=False, blank=False)
 
     def __str__(self):
-        return " || ".join([self.place_name, self.date_time.strftime("%Y/%m/%d, %H:%M:%S"), str(self.spender), str(self.total_spent)])
+        return " | ".join([self.place_name, self.date_time.strftime("%Y/%m/%d, %H:%M:%S"), str(self.creator), str(self.total_spent)])
 
 
 class Tag(models.Model):
     tag_name = models.CharField(max_length=200)
-    created = models.DateTimeField(auto_now_add=True)
+    default_tag = models.BooleanField(default=True, null=False, blank=False)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
     id = models.AutoField(primary_key=True, unique=True, editable=False)
 
     def __str__(self):
         return self.tag_name
+
+
+class Payment(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    payer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    split_ratio = models.FloatField()
+    currency = models.ForeignKey('Currency', editable=True, on_delete=models.SET_NULL, null=True)
+    paid_amount = models.IntegerField(default=0, editable=True, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True, editable=False, null=True, blank=True)
+    last_updated = models.DateTimeField(editable=True, default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.AutoField(primary_key=True, unique=True, editable=False)
+
+    def __str__(self):
+        return ' | '.join([self.session.title, self.date.strftime('%Y/%m/%d'), str(self.payer), str(self.paid_amount)])
+        # return ' | '.join([self.session.title, self.date.strftime('%Y/%m/%d'), str(self.payer), str(self.paid_amount) + " " + (self.currency.abbreviation if self.currency && self.currency.abbreviation else "") ])
+
+
+class Currency(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False, editable=True)
+    symbol = models.CharField(max_length=2, null=False, blank=False, editable=True)
+    abbreviation = models.CharField(max_length=3, null=False, blank=False, editable=True)
+    coefficient = models.FloatField(editable=True, null=False, blank=False)
+    last_updated = models.DateTimeField(editable=True, default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.AutoField(primary_key=True, unique=True, editable=False)
+
+    def __str__(self):
+        return ' | '.join([self.name, str(self.coefficient)])
